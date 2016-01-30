b8d5137790913f118f862b4f9fe5e5d808e4de6c
Christoph Witzany <christoph@web.crofting.com>
2016-01-30 23:57:07 +0100

    Make compatible with caldavzap

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 cp scripts/config /etc/radicale/config
+cp scripts/rights /etc/radicale/rights
 echo ldap_base = ou=Users,dc=`echo ${CLOUDFLEET_DOMAIN} | sed s/\\\./,dc=/g` >> /etc/radicale/config
 python -u /opt/cloudfleet/app/radicale.py
